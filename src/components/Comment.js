362435823fa964c5496c0c95ae3991a098de0975
IVAN <ivanbatur5g@gmail.com>
2021-01-09 22:14:39 +0100

    Deleting comments works, need to make them disappear when deleted

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import dayjs from "dayjs"
+import axios from "axios";
+import {useParams} from "react-router-dom"
+import {useAuth} from "../context/AuthContext"
 var relativeTime = require('dayjs/plugin/relativeTime')
 dayjs.extend(relativeTime)
 export default function Comment({comment}) {
+    const {token} = useAuth()
+    const params = useParams()
+    const [error, setError] = useState()
+    const handleDelete = e => {
+        axios.defaults.headers.common['Authorization'] = token;
+        axios.delete("http://localhost:4000/posts/"+ params.postId + "/comments/" + comment._id)
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(err => {
+            setError(err)
+        })
+    }
     return (
         <div className="comment">
+            {error && <span>{error}</span>}
             <h2>By: {comment.author}</h2>
             <p>{comment.body}</p>
             <small>{dayjs(comment.created_at).fromNow()}</small>
+            <button onClick={handleDelete}>Delete Comment</button>
         </div>
     )
 }
