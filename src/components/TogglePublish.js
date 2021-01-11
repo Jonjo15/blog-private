1fa74cbe6097a7c1b2488dde42bf0c0c6a2e807b
IVAN <ivanbatur5g@gmail.com>
2021-01-11 11:43:31 +0100

    Publishing fixed

--- src/components/TogglePublish.js
+++ src/components/TogglePublish.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from "axios"
 import {useAuth} from "../context/AuthContext"
 export default function TogglePublish({post, setPosts}) {
+    const [published, setPublished] = useState(null)
     const {token} = useAuth()
+    useEffect(() => {
+        setPublished(post.published)
+    }, [post])
+    if (published === null) return (<div>...loading</div>)
+    
     const handleClick = e => {
         axios.defaults.headers.common['Authorization'] = token;
-        const verb = post.published ? "unpublish" : "publish"
+        const verb = published ? "unpublish" : "publish"
         const route = "http://localhost:4000/posts/" + post._id + "/" + verb
         axios.put(route)
         .then(res => {
@@ -17,13 +23,15 @@ export default function TogglePublish({post, setPosts}) {
                     }
                     return p
                 })
+                
             })
+            setPublished(currPublished => !currPublished)
         })
         .catch(err => {
             console.error(err)
         })
     }
     return (
-        <button onClick={handleClick}>{post.published ? "Unpublish" : "Publish"}</button>
+        <button onClick={handleClick}>{published ? "Unpublish" : "Publish"}</button>
     )
 }
